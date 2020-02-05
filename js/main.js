c260f14aa817d80814f808052e779846bbb1f72e
LightFortune <konstantinkuznetsovkh@gmail.com>
2020-02-05 15:39:21 +0200

    04022020153931

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView:2,//how you see card in slider
+    loop: true, //infinity
+    navigation: {
+        nextEl: '.arrow'//button next custom       
+      },
+});
